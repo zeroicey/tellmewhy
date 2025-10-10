181791527193f03e8db7394f6b2d67fa69db6b4e
zeroicey <zeroicey.hp@outlook.com>
2025-10-10 17:27:15 +0800

    feat(question): 添加登出按钮功能
    在问题页面添加登出按钮，点击后清除认证状态并跳转到登录页

--- src/pages/question.tsx
+++ src/pages/question.tsx
@@ -1,10 +1,23 @@
+import { Button } from "@/components/ui/button";
 import { useQuestionQuery } from "@/hooks/use-question-query";
+import useAuthStore from "@/stores/auth";
+import { useNavigate } from "react-router";
 
 export default function QuestionPage() {
+  const navigate = useNavigate();
   const { data: questions } = useQuestionQuery();
 
   return (
     <div className="w-full h-full flex justify-center items-center">
+      <Button
+        variant={"default"}
+        onClick={() => {
+          useAuthStore.getState().signOut();
+          navigate("/signin");
+        }}
+      >
+        Log out
+      </Button>
       <ul>
         {questions?.map((question) => {
           return <li key={question.id}>{question.title}</li>;
