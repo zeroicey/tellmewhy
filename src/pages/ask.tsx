9eebc000ba8cea72bb4be9fbe158dc604286989f
Jerry-Guo1020 <2593517388@qq.com>
2025-10-11 18:27:44 +0800

    feat(ask): 实现提问页面表单提交功能
    添加提问表单的输入验证和提交逻辑，包括标题和内容的输入框，以及通过Supabase将数据插入数据库。提交成功后跳转到问题列表页面。

--- src/pages/ask.tsx
+++ src/pages/ask.tsx
@@ -1,3 +1,54 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { supabase } from "@/lib/supabase";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { toast } from "sonner";
+
 export default function AskPage() {
-  return <div>ask</div>;
+  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleAsk = async (e: MouseEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    if (title.trim() === "") {
+      toast.error("标题不能为空！");
+      return;
+    }
+    const { data, error } = await supabase
+      .from("questions")
+      .insert([{ title: title , content: content}])
+      .select()
+    /*键值对：e.target.value */
+      console.log(data);
+      console.log(error);
+      
+      navigate("/questions");
+  };
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center ">
+      <form className="flex items-center flex-col">
+        <div className="mb-5 text-3xl ">this is the ask page</div>
+        <Input
+          type="text"
+          placeholder="请输入标题"
+          className="w-200"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="请输入内容"
+          className="w-200 h-50 px-5 py-3 my-2 inline-block border border-gray-300 rounded-md box-border focus:bg-blue-100 focus:border-gray-700 focus:border-[3px] transition-colors duration-500 ease-in-out"
+        />
+        <Button type="submit" onClick={handleAsk}>
+          提交
+        </Button>
+      </form>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ export default function IndexPage() {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
       <Loader2 className="animate-spin" size={48} />
+      这个是一个首页一打开的而已
     </div>
   );
 }
